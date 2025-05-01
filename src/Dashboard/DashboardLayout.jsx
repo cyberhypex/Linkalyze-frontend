82e0050fd9f8d540e1996189dd1ff2ba0b704f13
Anshuman <chaoanxuman999@gmail.com>
2025-05-01 10:17:06 +0530

    Chart designed

--- src/Dashboard/DashboardLayout.jsx
+++ src/Dashboard/DashboardLayout.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { dummyData } from '../DummyData/data'
+import Graph from './Graph'
+import {useStoreContext} from '../contextApi/ContextApi'
+import { useFetchTotalClicks } from '../Hooks/useQuery'
 
 export function DashboardLayout(props) {
-    
-
+    const {token}=useStoreContext();
+    function onError(){
+        console.log("ERROR");
+    }
+    console.log(useFetchTotalClicks(
+        token,onError
+    ));
     return (
         <>
-            
+            <div className="lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]">
+                <div className="lg:w-[90%] w-full mx-auto py-16">
+                    <div className="h-96 relative">
+                        <Graph graphData={dummyData} />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
