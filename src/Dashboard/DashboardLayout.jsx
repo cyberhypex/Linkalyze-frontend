2cb18d8a52787627c72a72dac94d5475ccc3e097
Anshuman <chaoanxuman999@gmail.com>
2025-05-02 09:54:26 +0530

    Login and tested data real

--- src/Dashboard/DashboardLayout.jsx
+++ src/Dashboard/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { dummyData } from '../DummyData/data'
+
 import Graph from './Graph'
 import {useStoreContext} from '../contextApi/ContextApi'
 import { useFetchTotalClicks } from '../Hooks/useQuery'
@@ -9,15 +9,16 @@ export function DashboardLayout(props) {
     function onError(){
         console.log("ERROR");
     }
-    console.log(useFetchTotalClicks(
-        token,onError
-    ));
+  //  console.log(useFetchTotalClicks(
+    //    token,onError
+    //));
+    const {isLoading: loader, data: totalClicks} = useFetchTotalClicks(token, onError)
     return (
         <>
             <div className="lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]">
                 <div className="lg:w-[90%] w-full mx-auto py-16">
                     <div className="h-96 relative">
-                        <Graph graphData={dummyData} />
+                        <Graph graphData={totalClicks} />
                     </div>
                 </div>
             </div>
