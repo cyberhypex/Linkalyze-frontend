3411b7e0e885e369388fdefb5f5dcbc804a9bde7
Anshuman <chaoanxuman999@gmail.com>
2025-05-02 15:10:47 +0530

    User tested from postman as well

--- src/Dashboard/DashboardLayout.jsx
+++ src/Dashboard/DashboardLayout.jsx
@@ -15,13 +15,14 @@ export function DashboardLayout(props) {
     const {isLoading: loader, data: totalClicks} = useFetchTotalClicks(token, onError)
     return (
         <>
-            <div className="lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]">
+          <div className="lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]">
                 <div className="lg:w-[90%] w-full mx-auto py-16">
                     <div className="h-96 relative">
                         <Graph graphData={totalClicks} />
                     </div>
                 </div>
             </div>
+            
         </>
     )
 }
