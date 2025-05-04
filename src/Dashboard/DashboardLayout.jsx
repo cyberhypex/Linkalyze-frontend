39038682b52acaabda7efbfec8e770b087928316
Anshuman <chaoanxuman999@gmail.com>
2025-05-04 10:52:40 +0530

    Fixed Dummy data issue and put button

--- src/Dashboard/DashboardLayout.jsx
+++ src/Dashboard/DashboardLayout.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Graph from './Graph'
 import {useStoreContext} from '../contextApi/ContextApi'
 import { useFetchTotalClicks } from '../Hooks/useQuery'
+import { dummyData } from '../DummyData/data';
 
 export function DashboardLayout(props) {
     const {token}=useStoreContext();
@@ -18,9 +19,16 @@ export function DashboardLayout(props) {
           <div className="lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]">
                 <div className="lg:w-[90%] w-full mx-auto py-16">
                     <div className="h-96 relative">
-                        <Graph graphData={totalClicks} />
+                        <Graph graphData={dummyData} />
                     </div>
                 </div>
+            </div>
+            <div className='py-5 sm:text-end text-center'>
+                <button className='bg-custom-gradient px-4 py-2 rounded-md text-white'>
+                    Create a new Short URL
+
+                </button>
+
             </div>
             
         </>
