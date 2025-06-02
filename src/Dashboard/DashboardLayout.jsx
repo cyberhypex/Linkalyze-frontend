c181e09d83f46bb09e1ee4109c162cc234f3ff06
Anshuman <chaoanxuman999@gmail.com>
2025-06-02 23:30:11 +0530

    Handled data not available graph

--- src/Dashboard/DashboardLayout.jsx
+++ src/Dashboard/DashboardLayout.jsx
@@ -1,36 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import Graph from './Graph'
-import {useStoreContext} from '../contextApi/ContextApi'
-import { useFetchTotalClicks } from '../Hooks/useQuery'
+import Graph from './Graph';
+import { useStoreContext } from '../contextApi/ContextApi';
+import { useFetchTotalClicks } from '../Hooks/useQuery';
 import { dummyData } from '../DummyData/data';
+ // Assuming you forgot to import Loader
 
 export function DashboardLayout(props) {
-    const {token}=useStoreContext();
-    function onError(){
+    const { token } = useStoreContext();
+    const [shortenPopUp,setShortenPopUp]=useState(false)
+
+    function onError() {
         console.log("ERROR");
     }
-  //  console.log(useFetchTotalClicks(
-    //    token,onError
-    //));
-    const {isLoading: loader, data: totalClicks} = useFetchTotalClicks(token, onError)
+
+    const { isLoading: loader, data: totalClicks } = useFetchTotalClicks(token, onError);
+
     return (
         <>
-          <div className="lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]">
-                <div className="lg:w-[90%] w-full mx-auto py-16">
-                    <div className="h-96 relative">
-                        <Graph graphData={dummyData} />
-                    </div>
-                </div>
+            <div className="lg:px-14 sm:px-8 px-4 min-h-[calc(100vh-64px)]">
+                {loader ? (
+                    <p>Loading ....</p>
+                ) : (
+                    <>
+                        <div className="lg:w-[90%] w-full mx-auto py-16">
+                            <div className="h-96 relative">
+                                {totalClicks.length === 0 && (
+                     <div className="absolute flex flex-col  justify-center sm:items-center items-end  w-full left-0 top-0 bottom-0 right-0 m-auto">
+                     <h1 className=" text-slate-800 font-serif sm:text-2xl text-[18px] font-bold mb-1">
+                       No Data For This Time Period
+                     </h1>
+                     <h3 className="sm:w-96 w-[90%] sm:ml-0 pl-6 text-center sm:text-lg text-sm text-slate-600 ">
+                       Share your short link to view where your engagements are
+                       coming from
+                     </h3>
+                   </div>
+                )}
+                                <Graph graphData={totalClicks} />
+                            </div>
+                        </div>
+                        <div className='py-5 sm:text-end text-center'>
+                            <button className='bg-custom-gradient px-4 py-2 rounded-md text-white'
+                            onClick={()=>{
+                                setShortenPopUp(true)
+                            }}>
+                                Create a new Short URL
+                                
+                            </button>
+                        </div>
+                    </>
+                )}
             </div>
-            <div className='py-5 sm:text-end text-center'>
-                <button className='bg-custom-gradient px-4 py-2 rounded-md text-white'>
-                    Create a new Short URL
-
-                </button>
-
-            </div>
-            
         </>
-    )
+    );
 }
