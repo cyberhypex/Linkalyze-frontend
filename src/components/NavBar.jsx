a7ddd0e10e13a1e1bcbda00a583692298bcfb000
Anshuman <chaoanxuman999@gmail.com>
2025-06-05 13:54:53 +0530

    Implemented Logout functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { IoIosMenu } from "react-icons/io";
 import { RxCross2 } from "react-icons/rx";
+import { useStoreContext } from "../contextApi/ContextApi";
+import { button } from "framer-motion/client";
 const Navbar = () => {
   const navigate = useNavigate();
-  
+  const {token , setToken}=useStoreContext();
   const path = useLocation().pathname;
   const [navbarOpen, setNavbarOpen] = useState(false);
 
@@ -47,11 +49,33 @@ const Navbar = () => {
               About
             </Link>
           </li>
-          <Link to="/register">
+          {token &&(
+            <li className="hover:text-btnColor font-[500]  transition-all duration-150">
+            <Link
+              className={`${
+                path === "/dashboard" ? "text-white font-semibold" : "text-gray-200"
+              }`}
+              to="/about"
+            >
+             DashBoard
+            </Link>
+          </li>
+          )}
+          {!token && (
+            <Link to="/register">
               <li className=" sm:ml-0 -ml-1 bg-rose-700 text-white  cursor-pointer w-24 text-center font-semibold px-2 py-2 rounded-md  hover:text-slate-300   transition-all duration-150">
                 SignUp
               </li>
             </Link> 
+          )}
+          {token && (
+            <button
+            onClick={onLogOutHandler}
+            className=" sm:ml-0 -ml-1 bg-rose-700 text-white  cursor-pointer w-24 text-center font-semibold px-2 py-2 rounded-md  hover:text-slate-300   transition-all duration-150"
+            >
+              LogOut
+            </button>
+          )}
          
         </ul>
         <button
